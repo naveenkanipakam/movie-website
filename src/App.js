a7a72b3a6848a5c3b0dfe4ac19457f294a7b5e37
Naveen <naveenpillai015@gmail.comgit config --global user.name NaveenOmit --global to set the identity only in this repository.git commit -mAssignmentgit config --global user.email naveenpillai015@gmail.com>
2023-06-05 22:33:54 +0530

    movie-website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+// export default App;
+import React, { Component } from "react";
+import MoviesHeader from "./component/movieHeader";
+import Cards from "./component/code";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiData: [],
+      loader: true
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://www.omdbapi.com/?apikey=45f0782a&s=war/")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ apiData: json, loader: false });
+      });
+  }
+
+  render() {
+    // let {loader, apiData} = this.state
+    let { apiData } = this.state;
+    let { loader } = this.state;
+    if (!loader) {
+      console.log(apiData.Search);
+    }
+    return (
+      <div className="wrapper">
+        <MoviesHeader />
+        <div className="cards-wrapper">
+          {loader ? (
+            <h2 style={{ color: "white" }}>Loading...</h2>
+          ) : (
+            apiData.Search?.map((item) => (
+              <Cards Poster={item.Poster} Title={item.Title} />
+            ))
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
